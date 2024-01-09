b86a0134d25846292a8bd037ffe0ea068a9a16f8
Gray Forrester <gradyforrester3@gmail.com>
2024-01-08 23:07:55 -0500

    Testing home connection - 11:07pm

--- gnf5628_HW2_q1.cpp
+++ gnf5628_HW2_q1.cpp
@@ -4,5 +4,7 @@ int main() {
     std::cout << "Hello, World! Again!" << std::endl;
 
     std::cout << "Pushing from work: 11:05pm" << std::endl;
+
+    std::cout << "Pushing from home: 11:07om" << std::endl;
     return 0;
 }
