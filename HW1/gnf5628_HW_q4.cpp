d0af3c91e35ff457a828fd5f468663eb994a3802
Gray Forrester <gradyforrester3@gmail.com>
2024-01-10 22:13:43 -0500

    Creating the Skeleton for all of HW2

---
+++ HW1/gnf5628_HW_q4.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+// Created by Gray Forrester on 1/10/24.
+//
+
+/*
+Question 4:
+Write a program that reads from the user two positive integers, and prints the result of when we add, subtract multiply, divide, div and mod them.
+Your program should interact with the user exactly as it shows in the following example:
+
+ Please enter two positive integers, separated by a space:
+14 4
+
+14+4=18
+14–4=10
+ 4*4=56
+ 14/4=3.5
+ 14div4=3
+ 14mod4=2
+ */
+
+int main() {
+    std::cout << "hello from 4!";
+}
