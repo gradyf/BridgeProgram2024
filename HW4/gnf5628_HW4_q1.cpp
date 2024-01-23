8b352b1a03f9ec3e428bbcb0ef353d7d12409296
Gray Forrester <gradyforrester3@gmail.com>
2024-01-22 23:38:11 -0500

    Moving to HW4

--- HW4/gnf5628_HW4_q1.cpp
+++ HW4/gnf5628_HW4_q1.cpp
@@ -3,5 +3,16 @@ using namespace std;
 // Created by Gray Forrester on 1/22/24.
 //
 int main() {
-    cout << "Hello World";
+
+    int counter, limit;
+    cout << "Please enter an integer: ";
+    cin >> limit;
+
+    for(counter = 1; counter <= limit; counter++) {
+        cout << '*' * counter << endl;
+    }
+
+
+
+
 }
