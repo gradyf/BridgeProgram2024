daf2b13af73ff3f4eda6675c0e51e728d2b24d1e
Gray Forrester <gradyforrester3@gmail.com>
2024-01-22 21:50:13 -0500

    Working on HW3

--- HW3/gnf5628_HW3_q6.cpp
+++ HW3/gnf5628_HW3_q6.cpp
@@ -5,13 +5,38 @@
 #include <string>
 using namespace std;
 
+const double WEEKEND_RATE = 0.15;
+const double WEEKDAY_PEAK = 0.40;
+const double WEEKDAY_OFF = 0.25;
+
 int main() {
 
     int hour, minute;
 
+    double length, total;
+
     string day;
 
     char colon;
+
+    cout << "Please enter a date, time, and length:" << endl;
+
+    cin >> day;
+    cin >> hour >> colon >> minute;
+    cin >> length;
+
+    if (day == "Sa" || day == "Su") {
+        total = length * WEEKEND_RATE;
+        cout << total;
+    }
+    else if (hour <= 7 || (hour >= 18 && minute > 0)) {
+        total = length * WEEKDAY_OFF;
+        cout << total;
+    }
+    else {
+        total = length * WEEKDAY_PEAK;
+        cout << total;
+    }
 
 
 
