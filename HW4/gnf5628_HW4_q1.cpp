9a4916bc379151c2c7ef15f8548ffb684a86349c
Gray Forrester <gradyforrester3@gmail.com>
2024-01-23 08:10:47 -0500

    Continuing HW4

--- HW4/gnf5628_HW4_q1.cpp
+++ HW4/gnf5628_HW4_q1.cpp
@@ -4,14 +4,34 @@ using namespace std;
 //
 int main() {
 
-    int counter, limit;
-    cout << "Please enter an integer: ";
-    cin >> limit;
+    cout << "section a" << endl;
 
-    for(counter = 1; counter <= limit; counter++) {
-        cout << '*' * counter << endl;
+    int counter_a= 1, tracker_a = 0, total_a;
+
+    cout << "Please enter a positive integer: ";
+    cin >> total_a;
+
+    while (tracker_a < total_a) {
+        if (counter_a % 2 == 0) {
+            cout << counter_a << endl;
+            tracker_a++;
+        }
+        counter_a++;
     }
 
+    cout << "section b" << endl;
+
+    int tracker_b = 0, total_b;
+
+    cout << "Please enter a positive integer: ";
+    cin >> total_b;
+
+    for (int counter_b = 1; tracker_b < total_b; counter_b++) {
+        if (counter_b % 2 == 0) {
+            cout << counter_b << endl;
+            tracker_b++;
+        }
+    }
 
 
 
