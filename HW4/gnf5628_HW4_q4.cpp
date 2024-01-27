c38361e1cf3c9744b1130300ff9383370d219355
Gray Forrester <gradyforrester3@gmail.com>
2024-01-27 13:05:14 -0500

    HW4

--- HW4/gnf5628_HW4_q4.cpp
+++ HW4/gnf5628_HW4_q4.cpp
@@ -10,27 +10,65 @@ int main() {
     cout << "section a" << endl;
 
 
-    int temp_total = 0;
+    int temp_total_a= 0;
 
-    double total, length, power;
+    double total_a, length_a, power_a;
 
     cout << "Please enter the length of the sequence: ";
-    cin >> length;
+    cin >> length_a;
 
     cout << "Please enter the sequence: " << endl;
 
-    for (int i = 0; i < length; i++) {
+    for (int i = 0; i < length_a; i++) {
         int temp = 0;
         cin >> temp;
 
-        temp_total += temp;
+        temp_total_a += temp;
     }
 
-    power = (1/length);
+    power_a = (1 / length_a);
 
-    total = pow(temp_total,power);
+    total_a = pow(temp_total_a, power_a);
 
-    cout << "The geometric mean is: " << total;
+    cout << "The geometric mean is: " << total_a << endl;
+
+
+    cout << "section b" << endl;
+
+
+
+    cout << "Please enter a non-empty sequence of positive integers, each one in a separate line. End your\n"
+            "sequence by typing -1:" << endl;
+
+    int trigger_b = 0;
+
+    int temp_total_b = 0;
+
+    double total_b = 0, length_b = 0, power_b = 0;
+
+
+    while (trigger_b != -1) {
+        int temp_b;
+
+        cin >> temp_b;
+
+        if ( temp_b == -1) {
+            length_b--;
+            trigger_b = -1;
+        }
+        else {
+            temp_total_b += temp_b;
+        }
+
+        length_b++;
+    }
+    cout << temp_total_b;
+
+    power_b = (1 / length_b);
+
+    total_b = pow(temp_total_b, power_b);
+
+    cout << "The geometric mean is: " << total_b << endl;
 
 
 }
