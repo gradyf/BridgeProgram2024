92a854fae3e655dfb310080124034bac1ae59832
Gray Forrester <gradyforrester3@gmail.com>
2024-01-27 11:58:20 -0500

    HW4

--- HW4/gnf5628_HW4_q5.cpp
+++ HW4/gnf5628_HW4_q5.cpp
@@ -8,11 +8,13 @@ using namespace std;
 
 int main() {
 
-    int left_A;
-    int star;
-    int right_B;
 
-    int left_top_lines = 0;
+
+    int top_spaces_lines = 0;
+    int top_stars_lines;
+
+    int bottom_spaces_lines;
+    int bottom_stars_length =0;
 
     int length;
 
@@ -20,36 +22,59 @@ int main() {
 
     cin >> length;
 
+    top_stars_lines = 2*length - 1;
+
+    bottom_spaces_lines = length-1;
+
+
+
     // top half of diamond
     for (int x = 0; x < length; x++){
 
         // left spaces
-        for (int i = 0; i < left_top_lines; i++) {
-            cout << "A";
+        for (int i = 0; i < top_spaces_lines; i++) {
+            cout << " ";
         }
 
         // stars
-        for (int i = 0; i < length; i++) {
+        for (int i = top_stars_lines; i > 0; i--) {
             cout << "*";
         }
 
         // Right spaces
-        for (int i = 0; i < length; i++) {
-            cout << "B";
+        for (int i = 0; i < top_spaces_lines; i++) {
+            cout << " ";
         }
 
-
+        // end of line transition
         cout << endl;
-        left_top_lines++;
+        top_spaces_lines++;
+        top_stars_lines -= 2;
     }
 
     // bottom half of diamond
 
     for (int x = 0; x < length; x++){
-        for (int i = 0; i < length; i++) {
+
+        // Left Spaces
+        for (int i = bottom_spaces_lines; i > 0; i--) {
+            cout << " ";
+        }
+
+        // Stars
+        for (int i = 0; i <= bottom_stars_length; i++) {
             cout << "*";
         }
+
+        // right spaces
+        for (int i = bottom_spaces_lines; i > 0; i--) {
+            cout << " ";
+        }
+
+        // end of line transition
         cout << endl;
+        bottom_spaces_lines--;
+        bottom_stars_length += 2;
     }
 
 }
