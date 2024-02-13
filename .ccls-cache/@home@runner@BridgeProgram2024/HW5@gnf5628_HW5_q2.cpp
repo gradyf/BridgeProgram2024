7ec6538f0a71d2b8e2c12add9f97ac8028c02411
gradyf <gradyforrester3@gmail.com>
2024-02-13 18:58:27 +0000

    Testing Replit

---
+++ .ccls-cache/@home@runner@BridgeProgram2024/HW5@gnf5628_HW5_q2.cpp
@@ -0,0 +1,80 @@
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+
+int main() {
+
+    //Creating the random answer variable
+    int rand_ans;
+
+    //creating and initializing the bounds
+    int lower_bound = 1, upper_bound = 100;
+
+    //variable for the user to guess the random variable.
+    int guess;
+
+    //tracker variable for how many guesses there has been
+    int count = 5;
+
+    //setting the seed for the random quess variable
+    srand(time(0));
+
+    // initializing the random guess variable
+    rand_ans = (rand() % 100) + 1;
+
+
+
+    cout << "I thought of a number between 1 and 100! Try to guess it." << endl;
+
+
+    while (count > 0) {
+        cout << "Range: [" << lower_bound << ", " << upper_bound << "], "
+             << "Number of guesses left: " << count << endl;
+
+        cout << "Your guess: ";
+        cin >> guess;
+        count--;
+
+        if (guess == rand_ans) {
+            cout << "Correct! You guessed my number in " << (5 - count) << " guesses";
+            count = -1;
+            break;
+        }
+        else if (guess > rand_ans && guess == upper_bound) {
+            cout << "Wrong! My number is smaller." << endl;
+            upper_bound--;
+        }
+        else if (guess < rand_ans && guess == lower_bound) {
+            cout << "Wrong! My number is bigger." << endl;
+            lower_bound++;
+        }
+
+        else if (guess > rand_ans && guess <= upper_bound) {
+            cout << "Wrong! My number is smaller." << endl;
+            upper_bound = guess;
+        }
+        else if (guess < rand_ans && guess >= lower_bound) {
+            cout << "Wrong! My number is bigger." << endl;
+            lower_bound = guess;
+        }
+        else if (guess > rand_ans ) {
+            cout << "Wrong! My number is smaller." << endl;
+
+        }
+        else if (guess < rand_ans) {
+            cout << "Wrong! My number is bigger." << endl;
+
+        }
+
+
+    }
+
+
+    if (count == 0) {
+        cout << "Out of guesses! My number is " << rand_ans;
+    }
+}
