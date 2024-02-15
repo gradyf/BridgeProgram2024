dff084c088173c819a059ef1b2545cfeaa6de617
Gray Forrester <gradyforrester3@gmail.com>
2024-02-15 09:29:41 -0500

    HW6

--- HW6/gnf5628_HW6_q97.cpp
+++ HW6/gnf5628_HW6_q97.cpp
@@ -1,3 +1,49 @@
 //
-// Created by Gray Forrester on 2/15/24.
-//
+// Question 3:
+
+// a)
+
+/*
+Implement a function:
+  int printMonthCalender(int numOfDays, int startingDay)
+This function is given two parameters:
+  numOfDays - The number of days in the month
+  startingDay – a number 1-7 that represents the day in the week of the first day
+          in that month (1 for Monday, 2 for Tuesday, 3 for Wednesday, etc.).
+
+The function should:
+  Print a formatted monthly calendar of that month
+     Return a number 1-7 that represents the day in the week of the last day in that
+        month
+
+Formatting Notes:
+  The output should include a header line with the days’ names.
+  Columns should be spaced by a Tab
+
+
+ */
+
+// b)
+
+/*
+ *
+ * A method for determining if a year is a leap year in the Gregorian calendar system
+ * is to check if it is divisible by 4 but not by 100, unless it is also divisible by 400.
+For example, 1896, 1904, and 2000 were leap years but 1900 was not.
+Write a function that takes in a year as input and return true if the year is
+ a leap year, return false otherwise.
+ *
+ *
+ */
+
+/*
+ * c) Implement a function
+ * void printYearCalender(int year, int startingDay)
+This function is given two parameters:
+year – an integer that represents a year (e.g. 2016)
+startingDay – a number 1-7 that represents the day in the week of 1/1 in that
+year (1 for Monday, 2 for Tuesday, 3 for Wednesday, etc.).
+ *
+ *
+ *
+ */
