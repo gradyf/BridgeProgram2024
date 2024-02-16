c3ad6bcb6ea10d4ec4ab613e8de02a86ce07b296
gradyf <gradyforrester3@gmail.com>
2024-02-16 10:35:48 -0500

    Working ahead on HW6

--- HW6/gnf5628_HW6_q3.cpp
+++ HW6/gnf5628_HW6_q3.cpp
@@ -2,4 +2,35 @@
 // Created by Gray Forrester on 2/16/24.
 //
 
+#include <iostream>
+using namespace std;
 
+double eApprox(int n);
+
+int main () {
+
+    cout.precision(30);
+
+    for (int n = 1; n <= 15; n++) {
+        cout << "n = " << n << '\t' << eApprox(n) << endl;
+    }
+
+    return 0;
+}
+
+//This Function returns e calculated at the n+1 addends
+double eApprox(int n) {
+    double sum = 1; // e stars with 1
+
+    //builds the outer addend loop
+    for (int addened = 0; addened < n; addened++){
+        int temp_denom = 0;
+        //builds the factoral for the denominator
+        for (int den = 1; den <= addened; den++) {
+            temp_denom += den;
+        }
+        sum += 1.0/temp_denom;
+    }
+
+    return sum;
+}
