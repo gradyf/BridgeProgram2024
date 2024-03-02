bb57b34f2a900b66ce9d0ba39b76b4aa1d66d5cc
Gray Forrester <gradyforrester3@gmail.com>
2024-03-02 15:03:42 -0500

    HW8

--- HW8/gnf5628_HW8_q1.cpp
+++ HW8/gnf5628_HW8_q1.cpp
@@ -5,7 +5,60 @@
 #include <iostream>
 using namespace std;
 
+int minInArray(int arr[], int arrSize);
+void fillArray(int arr[], int arraySize);
+void printIndices(int arr[], int arraySize, int target);
+
 int main() {
 
-    cout << "TEST";
+    const int arraySize = 20;
+
+    int input[arraySize];
+
+    fillArray(input,arraySize);
+
+    int minimum = minInArray(input, arraySize);
+
+    cout << "Here is the array you entered: ";
+    for(int x = 0; x < arraySize; x++) {
+        cout << input[x] << " ";
+    }
+    cout << endl << "The minimum value is " << minimum
+    << " and it is located at the following indices: ";
+
+    printIndices(input,arraySize, minimum);
+
+
+
+}
+
+int minInArray(int arr[], int arraySize) {
+
+    int min = arr[0];
+
+    for (int x = 0; x < arraySize; x++) {
+        if(arr[x] < min) {
+            min = arr[x];
+        }
+    }
+
+    return min;
+
+}
+
+void fillArray(int arr[], int arraySize){
+    cout << "Please enter " << arraySize << " integers seperated by a space: ";
+    for (int x = 0; x < arraySize; x+= 1) {
+        cin >> arr[x];
+    }
+
+}
+
+void printIndices(int arr[], int arraySize, int target){
+    for (int x = 0; x < arraySize; x++){
+        if (arr[x] == target) {
+            cout << x << " ";
+        }
+
+    }
 }
