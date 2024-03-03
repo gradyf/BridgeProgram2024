3e187173bdd45034264ace4880b6437bec793fb6
Gray Forrester <gradyforrester3@gmail.com>
2024-03-03 09:13:32 -0500

    HW8

--- HW8/gnf5628_HW8_q3.cpp
+++ HW8/gnf5628_HW8_q3.cpp
@@ -4,8 +4,85 @@
 
 #include <iostream>
 
+void reverseArray(int arr[], int arrSize);
+void printArray(int arr[], int arrSize);
+void removeOdd(int arr[], int& arrSize);
+
 using namespace std;
 int main() {
 
-    cout << "TETS";
+    int arr1[10] = {9, 2, 14, 12, -3};
+    int arr1Size = 5;
+    int arr2[10] = {21, 12, 6, 7, 14};
+    int arr2Size = 5;
+    int arr3[10] = {3, 6, 4, 1, 12};
+    int arr3Size = 5;
+
+    cout << "Reversing Array 1: "  << endl;
+    printArray(arr1, arr1Size);
+    reverseArray(arr1, arr1Size);
+    printArray(arr1, arr1Size);
+
+
+    cout << endl << "Removing odds, Array 2: "  << endl;
+    printArray(arr2, arr2Size);
+    removeOdd(arr2, arr2Size);
+    printArray(arr2, arr2Size);
+
+//    cout << endl << "Split Parity, Array 3: "  << endl;
+//    printArray(arr3, arr3Size);
+//    splitParity(arr3, arr3Size);
+//    printArray(arr3, arr3Size);
+
+    cout << endl << "Reversing =, Array 4: " << endl;
+    int arr4[10] = {9, 2, 14, 12, -3, 8};
+    int arr4Size = 6;
+
+    printArray(arr4, arr4Size);
+    reverseArray(arr4, arr4Size);
+    printArray(arr4, arr4Size);
+
+    cout << endl << "reversing, Array 5: " << endl;
+    int arr5[10] = {9, 2, 14, 12, -3, 8,56};
+    int arr5Size = 7;
+
+    printArray(arr5, arr5Size);
+    reverseArray(arr5, arr5Size);
+    printArray(arr5, arr5Size);
+
+
+
+    return 0;
+}
+
+void printArray(int arr[], int arrSize){
+    int i;
+    for (i = 0; i < arrSize; i++) {
+        cout<<arr[i]<<' ';
+    }
+    cout<<endl;
+}
+
+void reverseArray(int arr[], int arrSize) {
+
+    int swap = arrSize - 1;
+
+    for (int x = 0; x <= (arrSize / 2 - 1); x++) {
+        int temp1 = arr[x];
+        int temp2 = arr[swap];
+
+        arr[x] = temp2;
+        arr[swap] = temp1;
+        swap--;
+    }
+
+}
+
+void removeOdd(int arr[], int& arrSize) {
+    for(int x = 0; x < arrSize; x++) {
+        if(arr[x] % 2 != 0) {
+            cout << arr[x] << endl;
+        }
+    }
+
 }
