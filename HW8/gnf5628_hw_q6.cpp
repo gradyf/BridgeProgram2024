173add20f1af50cb997362b9937f8b10ab888010
Gray Forrester <gradyforrester3@gmail.com>
2024-03-04 20:40:45 -0500

    HW8

--- HW8/gnf5628_hw_q6.cpp
+++ HW8/gnf5628_hw_q6.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-int findNumber(string str);
+bool isLetter(char chr);
 
 int main() {
 
@@ -16,54 +16,53 @@ int main() {
 
     getline(cin, input);
 
-    while (findNumber(input) != -1) {
-        input[findNumber(input)] = 'x';
+    int beg_ind_tracker = 0;
+    int end_ind_tracker;
+
+    while (beg_ind_tracker < input.length()-1) {
+
+        end_ind_tracker = input.find(" ",beg_ind_tracker);
+
+        int length = end_ind_tracker - beg_ind_tracker;
+
+        string subString = input.substr(beg_ind_tracker,length);
+
+        bool skip = false;
+        for (int x = 0; x < subString.length(); x++) {
+            if(isLetter(subString[x])) {
+                skip = true;
+                break;
+            }
+        }
+        if(!skip) {
+            for (int y = 0; y < subString.length(); y++) {
+                subString[y] = 'x';
+            }
+
+        }
+
+
+        cout << subString << " ";
+
+
+        if (end_ind_tracker == -1) {
+            break;
+        }
+
+        beg_ind_tracker = end_ind_tracker + 1;
+
     }
 
-    cout << input;
 
 }
 
-int findNumber(string str) {
-    if (str.find("1") != -1) {
-        return str.find("1");
-    }
-    else if (str.find("2") != -1) {
-        return str.find("2");
-    }
-    else if (str.find("3") != -1) {
-        return str.find("3");
-    }
-    else if (str.find("4") != -1) {
-        return str.find("4");
-    }
-    else if (str.find("5") != -1) {
-        return str.find("5");
-    }
-    else if (str.find("6") != -1) {
-        return str.find("6");
-    }
-    else if (str.find("7") != -1) {
-        return str.find("7");
-    }
-    else if (str.find("8") != -1) {
-        return str.find("8");
-    }
-    else if (str.find("9") != -1) {
-        return str.find("9");
-    }
-    else if (str.find("10") != -1) {
-        return str.find("10");
+
+bool isLetter(char chr) {
+    if (chr != '1' && chr != '2' && chr != '3' && chr != '4' && chr != '5'
+    && chr != '6' && chr != '7' &&  chr != '8' && chr != '9' && chr != '0') {
+        return true;
     }
     else {
-        return -1;
+        return false;
     }
-}
-
-bool partOfWord (string str) {
-    for (int x = 0; x < str.length(); x++) {
-        if (x)
-
-    }
-
 }
