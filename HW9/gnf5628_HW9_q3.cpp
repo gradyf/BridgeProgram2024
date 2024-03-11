ca0064e014471d8c1096522f5b13363a55477045
Gray Forrester <gradyforrester3@gmail.com>
2024-03-11 18:35:05 -0500

    HW9

--- HW9/gnf5628_HW9_q3.cpp
+++ HW9/gnf5628_HW9_q3.cpp
@@ -19,14 +19,23 @@ void getPosNums4(int* arr, int arrSize,
 int main() {
 
 
-    cout << "---------------Function1 Test--------------";
+    cout << "---------------Function1 Test--------------" << endl;
     int outPosArr1Size = 0;
     int arr1InputSize = 10;
 
     int arr1input[] = {3,-1,-3,0,6,4, 10, 20, -8, 1};
 
+    cout << "Old Array:";
+    for (int x = 0; x < arr1InputSize; x++) {
+        cout << arr1input[x] << " ";
+    }
+
+    cout << endl;
+
+
     int* arr1Test = getPosNums1(arr1input,arr1InputSize,outPosArr1Size);
 
+    cout << "New Array:";
     for (int x = 0; x < outPosArr1Size; x++) {
         cout << arr1Test[x] << " ";
     }
@@ -34,25 +43,40 @@ int main() {
     delete [] arr1Test;
 
 
-    cout << "---------------Function2 Test--------------";
+    cout << endl << "---------------Function2 Test--------------" << endl;
 
     int outPosArr2Size = 0;
     int arr2InputSize = 10;
 
+    cout << "outPosArr2Size: " << outPosArr2Size << endl;
+
+    int *outPosArr2SizePtr = &outPosArr2Size;
+
+    cout << "outPosArr2Size: " << outPosArr2Size << endl;
+
+
+
     int arr2input[] = {-8,-2,15,3,9,-7, 0, 0, 3, 1};
 
-    int* arr2Test = getPosNums1(arr1input,arr1InputSize,outPosArrSize);
+    cout << "outPosArr2Size: " << outPosArr2Size << endl;
 
-    for (int x = 0; x < outPosArr2Size; x++) {
-        cout << arr1Test[x] << " ";
+    cout << "Old Array:";
+    for (int x = 0; x < arr2InputSize; x++) {
+        cout << arr2input[x] << " ";
     }
 
-    delete [] arr1Test;
+    cout << endl;
 
+    int* arr2Test = getPosNums2(arr2input, arr2InputSize, outPosArr2SizePtr);
 
+    cout << "outPosArr2Size: " << outPosArr2Size << endl;
 
+    cout << "New Array:";
+    for (int x = 0; x < outPosArr2Size; x++) {
+        cout << arr2Test[x] << " ";
+    }
 
-
+    delete [] arr2Test;
 }
 
 
@@ -102,31 +126,37 @@ and updates the output parameter
  @param outPosArrSizePtr
 */
 
-// int* getPosNums2(int* arr, int arrSize, int* outPosArrSizePtr) {
-//    // determine how large the new size needs to be
-//    for (int x = 0; x < arrSize; x++) {
-//        if (arr[x] > 0) {
-//            outPosArrSize++;
-//        }
-//    }
-//
-//    //define the pointer //create new array based on the new size
-//    int* arr2 = new int [outPosArrSize];
-//
-//    //create a index tracker for the new array to be used to fill the new array
-//    int indTracker = 0;
-//
-//    //Fills the  new array with the positive values:
-//    for (int x = 0; x < arrSize; x++) {
-//        if (arr[x] > 0) {
-//            arr2[indTracker] = arr[x];
-//            indTracker++;
-//        }
-//    }
-//
-//    return arr2;
-//
-//}
+ int* getPosNums2(int* arr, int arrSize, int* outPosArrSizePtr) {
+    // determine how large the new size needs to be
+    int temp = 0;
+    for (int x = 0; x < arrSize; x++) {
+        if (arr[x] > 0) {
+            temp++;
+        }
+    }
+
+    cout << "New size: " << temp << endl;
+
+    outPosArrSizePtr = &temp;
+
+
+    //define the pointer //create new array based on the new size
+    int* arr2 = new int [*outPosArrSizePtr];
+
+    //create a index tracker for the new array to be used to fill the new array
+    int indTracker = 0;
+
+    //Fills the  new array with the positive values:
+    for (int x = 0; x < arrSize; x++) {
+        if (arr[x] > 0) {
+            arr2[indTracker] = arr[x];
+            indTracker++;
+        }
+    }
+
+    return arr2;
+
+}
 
 /**   updates the output parameter outPosArr
     with the base address of the array
