af541d751a3cc28d6a1524807083811f8d6d850d
Gray Forrester <gradyforrester3@gmail.com>
2024-03-12 19:33:57 -0500

    HW9

--- HW9/gnf5628_HW9_q3.cpp
+++ HW9/gnf5628_HW9_q3.cpp
@@ -70,6 +70,41 @@ int main() {
     }
 
     delete [] arr2Test;
+
+    cout << endl << "---------------Function3 Test--------------" << endl;
+
+    int outPosArr3Size = 0;
+    int arr3InputSize = 15;
+
+    int arr3input[] = {-7,2,67,-19,0,0, 0, 0, 3, 5, 6, 7, 0, 15, -25};
+
+    int arr3Test[] = {10,9,8,7,6};
+
+    int* outPos3Arr = arr3Test;
+
+
+    cout << endl << "outPosArr3Size: " << outPosArr3Size << endl;
+
+    cout << "Old Array: ";
+    for (int x = 0; x < arr3InputSize; x++) {
+        cout << arr3input[x] << " ";
+    }
+
+    cout << endl;
+
+    getPosNums3(arr3input, arr3InputSize, outPos3Arr, outPosArr3Size);
+
+    arr3Test = *outPos3Arr;
+
+    cout << endl << "outPosArr3Size: " << outPosArr3Size << endl;
+
+    cout << "New Array: ";
+    for (int x = 0; x < outPosArr3Size; x++) {
+        cout << arr3Test[x] << " ";
+    }
+
+//    delete [] arr3Test;
+
 }
 
 
@@ -161,8 +196,33 @@ and updates the output parameter
     @param outPosArr
     @param outPosArrSize
     */
-void getPosNums3(int* arr, int arrSize,
-                 int*& outPosArr, int& outPosArrSize) {
+void getPosNums3(int* arr, int arrSize, int* &outPosArr, int &outPosArrSize) {
+
+    // determine how large the new size needs to be
+    int temp = 0;
+    for (int x = 0; x < arrSize; x++) {
+        if (arr[x] > 0) {
+            temp++;
+        }
+    }
+
+    outPosArrSize = temp;
+
+    //define the pointer //create new array based on the new size
+    int* arr3 = new int [outPosArrSize];
+
+    //create a index tracker for the new array to be used to fill the new array
+    int indTracker = 0;
+
+    //Fills the  new array with the positive values:
+    for (int x = 0; x < arrSize; x++) {
+        if (arr[x] > 0) {
+            arr3[indTracker] = arr[x];
+            indTracker++;
+        }
+    }
+
+    outPosArr = arr3;
 
 }
 
