c25e77f68cca1618a6c6ba1aa13f6755f5e458f1
Gray Forrester <gradyforrester3@gmail.com>
2024-03-17 10:19:37 -0400

    HW10

---
+++ HW10/gnf5628_HW10_q1.cpp
@@ -0,0 +1,98 @@
+//
+// Created by Gray Forrester on 3/16/24.
+//
+#include <iostream>
+#include <string>
+using namespace std;
+
+string* createWordsArray(string sentence, int& outWordsArrSize);
+bool isLetter(char character);
+
+int main() {
+    int sentsize = 0;
+
+    string sentence;
+
+    cout << "Please enter a line of text: ";
+
+    getline(cin, sentence);
+
+    string* strArr = createWordsArray(sentence, sentsize);
+
+    cout << "sentsize: " << sentsize << endl;
+
+    cout << "Sentence: " << sentence << endl;
+
+    cout << "Sentence Array: " << endl;
+
+    for (int x = 0; x < sentsize; x++) {
+        cout << strArr[x] << " ";
+    }
+
+    delete[] strArr;
+
+
+}
+
+bool isLetter(char character) {
+
+    //if statement checking if the character is between upper case ASCII values
+    if((int) character >= 65 && character <= 90){
+        return true;
+    }
+        // else if statement checking if the character is between lowercase ASCII values
+    else if ((int) character >= 97 && character <= 122) {
+        return true;
+    }
+        // if it's not any of the above, return False
+    else {
+        return false;
+    }
+
+}
+
+string* createWordsArray(string sentence, int& outWordsArrSize) {
+
+    // counting the number of words
+    for (int x = 1; x < sentence.length(); x++) {
+
+        if ((x == sentence.length()-1) && (isLetter(sentence[x]))) {
+            outWordsArrSize++;
+        }
+
+        else if((!isLetter(sentence[x]) && (isLetter(sentence[x-1])))){
+
+            outWordsArrSize++;
+        }
+
+    }
+
+    //creating an array for the strings
+    string* strarr = new string[outWordsArrSize];
+
+    int index = 0;
+
+    int beg_ind_tracker = 0;
+    int end_ind_tracker;
+
+    while (beg_ind_tracker < sentence.length()-1) {
+
+        end_ind_tracker = sentence.find(" ",beg_ind_tracker);
+
+        int length = end_ind_tracker - beg_ind_tracker;
+
+        string subString = sentence.substr(beg_ind_tracker,length);
+
+        strarr[index] = subString;
+
+        if (end_ind_tracker == -1) {
+            break;
+        }
+
+        beg_ind_tracker = end_ind_tracker + 1;
+        index++;
+
+    }
+
+    return strarr;
+}
---
+++ HW10/gnf5628_HW10_q2.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Gray Forrester on 3/16/24.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int* findMissing(int arr[], int n, int& resArrSize);
+
+int main() {
+    cout << "test";
+
+}
+
+int* findMissing(int arr[], int n, int& resArrSize) {
+
+}
