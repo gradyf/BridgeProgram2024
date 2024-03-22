3b150d166ec65df1a6514e67476fce8a90b49c7e
Gray Forrester <gradyforrester3@gmail.com>
2024-03-22 18:10:52 -0400

    HW10

--- HW10/gnf5628_HW10_q3.cpp
+++ HW10/gnf5628_HW10_q3.cpp
@@ -8,42 +8,24 @@
 
 using namespace std;
 
-void main1(vector<int> &vect, int search);
-void expandArray(int arr[], int &arrSize, int input);
-void main2(int arr[], int arrSize, int search);
+void searchVect(vector<int> &vect, int search);
+void expandArray(int** arr, int &arrSize, int input);
+void searchArray(int arr[], int arrSize, int search);
+void main2();
+void main1();
 
 int main(){
 
-    int input, search, arrSize = 0;
+    main1();
 
-    vector<int> numVect;
+    cout << endl;
 
-    int numArr[0];
+    main2();
 
-    cout << "Please enter a sequence of positive integers, each in a separate line.";
-    cout << endl << "End your input by typing -1." << endl;
-
-    while (true) {
-        cin >> input;
-
-        if (input < 0) {
-            break;
-        }
-
-        numVect.push_back(input);
-//        expandArray(numArr, arrSize, input);
-
-    }
-
-    cout << "Please enter a number you want to search." << endl;
-    cin >> search;
-
-    main1(numVect, search);
-//    main2(numArr, arrSize, search);
-
+    return 0;
 }
 
-void main1(vector<int> &vect, int search) {
+void searchVect(vector<int> &vect, int search) {
     vector<int> output;
     bool inList = false;
 
@@ -75,27 +57,103 @@ void main1(vector<int> &vect, int search) {
 
 }
 
-void expandArray(int arr[], int &arrSize, int input) {
-    int arrTemp[arrSize + 1];
+void main1(){
+    int input, search;
 
-    for (int x = 0; x < arrSize; x++) {
-        arrTemp[x] = arr[x];
+    vector<int> numVect;
+
+    cout << "Vector Search: Please enter a sequence of positive integers, each in a separate line.";
+    cout << endl << "End your input by typing -1." << endl;
+
+    while (true) {
+        cin >> input;
+
+        if (input < 0) {
+            break;
+        }
+
+        numVect.push_back(input);
+
     }
 
-    arrSize++;
+    cout << "Vector Search: Please enter a number you want to search." << endl;
+    cin >> search;
+
+    searchVect(numVect, search);
+
+}
+
+void expandArray(int** arr, int &arrSize, int input) {
+
+    int* arrTemp = new int [arrSize + 1];
+
+    for (int x = 0; x < arrSize; x++) {
+        arrTemp[x] = (*arr)[x];
+    }
 
     arrTemp[arrSize] = input;
+    arrSize++;
 
-//    for (int x = 0; x < arrSize; x++) {
-//        arr[x] = arrTemp[x];
-//    }
+    delete [] *arr;
+    *arr = arrTemp;
+
+}
+
+void searchArray(int arr[], int arrSize, int search) {
+    int output[arrSize];
+    bool inList = false;
+    int index = 0;
+
+    for (int x = 0; x < arrSize; x++) {
+        if (arr[x] == search) {
+            output[index] = x;
+            index++;
+            inList = true;
+        }
+    }
+
+    if (inList) {
+        cout << search << " shows in lines ";
+        for (int x = 0; x < index; x++) {
+
+            if (x == index - 1) {
+                cout << output[x] + 1 << ".";
+            } else {
+                cout << output[x] + 1<< ", ";
+            }
+        }
+    }
+    else{
+        cout << search << " is not in the list.";
+    }
 
 
 }
 
-void main2(int arr[], int arrSize, int search) {
+void main2() {
+    int input, search, arrSize = 0;
 
 
+    int* numArr = new int[arrSize];
+
+    cout << "Array Search: Please enter a sequence of positive integers, each in a separate line.";
+    cout << endl << "End your input by typing -1." << endl;
+
+    while (true) {
+        cin >> input;
+
+        if (input < 0) {
+            break;
+        }
+
+        expandArray(&numArr, arrSize, input);
+
+    }
+
+    cout << "Array Search: Please enter a number you want to search." << endl;
+    cin >> search;
+
+    searchArray(numArr, arrSize, search);
 
 }
 
