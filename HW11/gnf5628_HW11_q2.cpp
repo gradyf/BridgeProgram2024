3f015f86bf735fbe48c21fe631f4938287570b2c
Gray Forrester <gradyforrester3@gmail.com>
2024-03-25 23:08:05 -0400

    HW11

--- HW11/gnf5628_HW11_q2.cpp
+++ HW11/gnf5628_HW11_q2.cpp
@@ -9,6 +9,7 @@ bool isSorted(int arr[], int arrSize);
 
 int main() {
 
+
     int arr1[] = {1,-1,5,-3,8,9};
     int arr1size = 6;
 
@@ -24,7 +25,8 @@ int main() {
         cout << endl <<  "arr1 is not sorted" << endl;
     }
 
-
+//////////////////////////////////////////////////////////////
+    cout << endl;
     int arr2[] = {1,3,4,6,8,9};
     int arr2size = 6;
 
@@ -38,6 +40,40 @@ int main() {
     }
     else {
         cout << endl <<  "arr2 is not sorted" << endl;
+    }
+
+    //////////////////////////////////////////////////////////////
+    cout << endl;
+    int arr3[] = {1,3,10,6,8,9};
+    int arr3size = 6;
+
+    cout << "arr3 is: ";
+    for (int x = 0; x < arr3size; x++) {
+        cout << arr3[x] << " ";
+    }
+
+    if(isSorted(arr3, arr3size)) {
+        cout << endl << "arr3 is sorted" << endl;
+    }
+    else {
+        cout << endl <<  "arr3 is not sorted" << endl;
+    }
+
+    //////////////////////////////////////////////////////////////
+    cout << endl;
+    int arr4[] = {1,3,3,3,3,9};
+    int arr4size = 6;
+
+    cout << "arr4 is: ";
+    for (int x = 0; x < arr4size; x++) {
+        cout << arr4[x] << " ";
+    }
+
+    if(isSorted(arr4, arr4size)) {
+        cout << endl << "arr4 is sorted" << endl;
+    }
+    else {
+        cout << endl <<  "arr4 is not sorted" << endl;
     }
 
 
@@ -60,6 +96,7 @@ int sumOfSquares(int arr[], int arrSize) {
     }
 }
 
+
 bool isSorted(int arr[], int arrSize) {
 
     if (arrSize == 1) {
@@ -67,8 +104,8 @@ bool isSorted(int arr[], int arrSize) {
     }
     else {
         if (arr[arrSize-2] > arr[arrSize-1]) {
-            cout << endl << "Index: " << arrSize - 2 << " is " <<  arr[arrSize-2]
-            << " which is greater than index " << arrSize - 1 << " which is " << arr[arrSize-1] << endl;
+//            cout << endl << "Index: " << arrSize - 2 << " is " <<  arr[arrSize-2]
+//            << " which is greater than index " << arrSize - 1 << " which is " << arr[arrSize-1] << endl;
             return false;
         }
         if(isSorted(arr, arrSize-1)) {
