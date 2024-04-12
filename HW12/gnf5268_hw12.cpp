4425ae898f9b7a6547205d34f7bc2a62f6ff24c9
Gray Forrester <gradyforrester3@gmail.com>
2024-04-12 19:52:01 -0400

    HW12

--- HW12/gnf5268_hw12.cpp
+++ HW12/gnf5268_hw12.cpp
@@ -1,71 +1,152 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 class Money {
 public:
-    friend Money operator+(const Money &amount1, const Money &amount2);
-    //Returns the sum of the values of amount1 and amount2.
+    friend Money operator+(const Money &amount1, const Money &amount2) {
+        Money temp;
+        temp.all_cents = amount1.all_cents + amount2.all_cents;
+        return temp;
+    }
 
-    friend Money operator-(const Money &amount1, const Money &amount2);
-    //Returns amount 1 minus amount2.
+    friend Money operator-(const Money &amount1, const Money &amount2) {
+        Money temp;
+        temp.all_cents = amount1.all_cents - amount2.all_cents;
+        return temp;
+    }
 
-    friend Money operator-(const Money &amount);
-    //Returns the negative of the value of amount.
+    friend Money operator-(const Money &amount) {
+        Money temp;
+        temp.all_cents = -amount.all_cents;
+        return temp;
+    }
 
-    friend bool operator==(const Money &amount1, const Money &amount2);
-    //Returns true if amount1 and amount2 have the same value; false otherwise.
+    friend bool operator==(const Money &amount1, const Money &amount2) {
+        return (amount1.all_cents == amount2.all_cents);
+    }
 
-    friend bool operator<(const Money &amount1, const Money &amount2);
-    //Returns true if amount1 is less than amount2; false otherwise.
+    friend bool operator<(const Money &amount1, const Money &amount2) {
+        return false;
+    }
 
-    Money(long dollars, int cents);
-    //Initializes the object so its value represents an amount with
-    //the dollars and cents given by the arguments. If the amount
-    //is negative, then both dollars and cents should be negative.
+    static int digit_to_int(char c) {
+        return (static_cast<int>(c) - static_cast<int>('0'));
+    }
 
-    Money(long dollars);
-    //Initializes the object so its value represents $dollars.00.
+    Money(long dollars, int cents) {
 
-    Money();
-    //Initializes the object so its value represents $0.00.
+        if (
+                dollars * cents <
+                0) {
+            cout << "Illegal values for dollars and cents.\n";
+            exit(1);
+        }
+        all_cents = dollars * 100 + cents; //If one is negative and one is positive
+    }
 
-    double get_value() const;
-    //Returns the amount of money recorded in the data portion of the calling
-    //object.
+    Money(long dollars) : all_cents(dollars * 100) {}
 
-    friend istream &operator>>(istream &ins, Money &amount);
-    //Overloads the >> operator so it can be used to input values of type
-    //Money. Notation for inputting negative amounts is as in -$100.00.
-    //Precondition: If ins is a file input stream, then ins has already been
-    //connected to a file.
+    Money() : all_cents(0) {}
 
+    double get_value() const {
+        return (all_cents * 0.01);
+    }
 
-    friend ostream &operator<<(ostream &outs, const Money &amount);
-    //Overloads the << operator so it can be used to output values of type
-    //Money. Precedes each output value of type Money with a dollar sign.
-    //Precondition: If outs is a file output stream, then outs has already been
-    //connected to a file.
+    friend istream &operator>>(istream &ins, Money &amount) {
+        char one_char, decimal_point,
+                digit1, digit2; //digits for the amount of cents
+        long dollars;
+        int cents;
+        bool negative;//set to true if input is negative.
+
+        ins >> one_char;
+        if (one_char == '-') {
+            negative = true;
+            ins >> one_char; //read '$'
+        } else
+            negative = false;
+        //if input is legal, then one_char == '$'
+
+        ins >> dollars >> decimal_point >> digit1 >> digit2;
+
+        if (one_char != '$' || decimal_point != '.'
+            || !isdigit(digit1) || !isdigit(digit2)) {
+            cout << "Error illegal form for money input\n";
+            exit(1);
+        }
+
+        cents = digit_to_int(digit1) * 10 + digit_to_int(digit2);
+
+        amount.all_cents = dollars * 100 + cents;
+        if (negative)
+            amount.all_cents = -amount.all_cents;
+
+        return ins;
+    }
+
+    friend ostream &operator<<(ostream &outs, const Money &amount) {
+        long positive_cents, dollars, cents;
+        positive_cents = labs(amount.all_cents);
+        dollars = positive_cents / 100;
+        cents = positive_cents % 100;
+
+        if (amount.all_cents < 0)
+            outs << "-$" << dollars << '.';
+        else
+            outs << "$" << dollars << '.';
+
+        if (cents < 10)
+            outs << '0';
+        outs << cents;
+
+        return outs;
+    }
+
 private:
     long all_cents;
+
 };
-    class Checkbook {
+
+class Check {
 
 public:
-    Checkbook(int checkNumber, double checkAmount, bool checkStatus);
-    Checkbook(int checkNumber, double checkAmount);
-    Checkbook(int checkNumber);
-    Checkbook();
+    Check(int number, Money amount, bool status) {
+        checkNumber = number;
+        checkAmount = amount;
+        checkStatus = status;
+    }
 
-    int get_checkNumber();
-    Money get_checkAmount();
-    bool get_checkStatus();
+    Check() {
+        checkNumber = 0;
+        checkAmount = 0;
+        checkStatus = 0;
+    }
 
-    void set_checkNumber(int number);
-    void set_checkAmount(Money amount);
-    void set_checkStatus(bool status);
+    int get_checkNumber() {
+        return checkNumber;
+    }
 
-private:
+    Money get_checkAmount() {
+        return checkAmount;
+    }
+
+    bool get_checkStatus() {
+        return checkStatus;
+    }
+
+    void set_checkNumber(int number) {
+        checkNumber = number;
+    }
+
+    void set_checkAmount(Money amount) {
+        checkAmount = amount;
+    }
+
+    void set_checkStatus(bool status) {
+        checkStatus = status;
+    }
 
     int checkNumber;
     Money checkAmount;
@@ -73,102 +154,77 @@ private:
 
 };
 
-// Money Class declaration here:
+
+int main() {
+
+    int checkNumber, checkbookSize = 0, depositsSize = 0;
+
+    Money checkAmount, cashedSumAmount, priorBalance, unCashedSumAmount;
+
+    bool checkStatus;
+
+    Check* checkbook = new Check[checkbookSize];
+
+    Money* deposits = new Money[depositsSize];
+
+    cout << "Please enter a check number, amount on the check [Include the dollar sign], and whether or not the " <<
+         "check has been cashed, indicated by a 1(Yes) or 0(No). Enter a 0 for all three when done inputting checks" << endl;
+
+    while (true) {
+        cin >> checkNumber >> checkAmount >> checkStatus;
+
+        if (checkNumber == 0 && checkStatus == 0 && checkNumber == 0) {
+            break;
+        }
+        checkbookSize++;
+
+        Check* newCheckbook = new Check[checkbookSize];
+
+        for (int x = 0; x < checkbookSize - 1; x++) {
+            newCheckbook[x] = checkbook[x];
+        }
+
+
+        newCheckbook[checkbookSize - 1].checkNumber = checkNumber;
+        newCheckbook[checkbookSize - 1].checkAmount = checkAmount;
+        newCheckbook[checkbookSize - 1].checkStatus = checkStatus;
+
+        delete [] checkbook;
+        checkbook = newCheckbook;
+
+    }
+
+    cout << "The sum of the cashed checks is: " << endl;
+
+    for (int i = 0; i < checkbookSize; i++ ) {
+        if(checkbook[i].checkStatus == 1) {
+            cashedSumAmount = cashedSumAmount + checkbook[i].checkAmount;
+        }
+    }
+
+    cout << cashedSumAmount << endl;
+
+    cout << "The sum of the uncashed checks is: " << endl;
+
+    for (int i = 0; i < checkbookSize; i++ ) {
+        if(checkbook[i].checkStatus == 0) {
+            unCashedSumAmount = unCashedSumAmount + checkbook[i].checkAmount;
+        }
+    }
+
+    cout << unCashedSumAmount << endl;
+
+    cout << "Please enter the prior balance amount (Format: $#.##): " << endl;
+
+    cin >> priorBalance;
 
 
 
-int main(){
-    cout << endl << "-----------------------CB1-----------------------" << endl;
-    Checkbook cb1(1234, 105.43, true);
-
-    cout << "Amount Before: " << cb1.get_checkAmount() << endl;
-    cb1.set_checkAmount(123.44);
-    cout << "Amount After: " << cb1.get_checkAmount() << endl;
-    cout << "Number Before: " << cb1.get_checkNumber() << endl;
-    cb1.set_checkNumber(67128);
-    cout << "Number After: " << cb1.get_checkNumber() << endl;
-    cout << "Status Before: " << cb1.get_checkStatus() << endl;
-    cb1.set_checkStatus(false);
-    cout << "Status After: " << cb1.get_checkStatus() << endl;
-
-    cout << endl << "-----------------------CB2-----------------------" << endl;
-
-    Checkbook cb2(9876, 204.12);
-
-    cout << "Amount: " << cb2.get_checkAmount() << endl;
-    cout << "Number: " << cb2.get_checkNumber() << endl;
-    cout << "Status: " << cb2.get_checkStatus() << endl;
 
 
 
-    cout << endl << "-----------------------CB3-----------------------" << endl;
-
-    Checkbook cb3(57493 );
-
-    cout << "Amount: " << cb3.get_checkAmount() << endl;
-    cout << "Number: " << cb3.get_checkNumber() << endl;
-    cout << "Status: " << cb3.get_checkStatus() << endl;
-
-    cout << endl << "-----------------------CB4-----------------------" << endl;
-
-    Checkbook cb4;
-
-    cout << "Amount: " << cb4.get_checkAmount() << endl;
-    cout << "Number: " << cb4.get_checkNumber() << endl;
-    cout << "Status: " << cb4.get_checkStatus() << endl;
-}
-
-// Three input contructor
-Checkbook::Checkbook(int number, double amount, bool status) {
-    checkNumber = number;
-    checkAmount = amount;
-    checkStatus = status;
-}
-
-// Two input contructor
-Checkbook::Checkbook(int number, double amount) {
-    checkNumber = number;
-    checkAmount = amount;
-    checkStatus = false;
-
-}
-
-// One input Constructor
-Checkbook::Checkbook(int number) {
-    checkNumber = number;
-    checkAmount = 0;
-    checkStatus = false;
-}
-
-//Default Constructor
-Checkbook::Checkbook() {
-    checkNumber = NULL;
-    checkAmount = 0;
-    checkStatus = false;
-}
 
 
-int Checkbook::get_checkNumber() {
-    return checkNumber;
-}
 
-bool Checkbook::get_checkStatus() {
-    return checkStatus;
-}
-
-double Checkbook::get_checkAmount() {
-    return checkAmount;
-}
-
-void Checkbook::set_checkNumber(int number) {
-    checkNumber = number;
-}
-
-void Checkbook::set_checkStatus(bool status) {
-    checkStatus = status;
-}
-
-void Checkbook::set_checkAmount(double amount) {
-   checkAmount = amount;
 }
 
