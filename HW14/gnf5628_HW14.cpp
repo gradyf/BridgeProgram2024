555b8bed6fc57cd788d7723b0a71f0abcb55fd1f
Gray Forrester <gradyforrester3@gmail.com>
2024-04-24 08:43:59 -0400

    HW13

--- HW14/gnf5628_HW14.cpp
+++ HW14/gnf5628_HW14.cpp
@@ -9,6 +9,7 @@ vector<T> minMax(vector<T> vect) {
     vector<T> answer;
     if (vect.size() == 1) {
         answer.push_back(vect[0]);
+        answer.push_back(vect[0]);
         return answer;
     } else if (vect.size() == 2) {
         if (vect[0] < vect[1]) {
@@ -25,12 +26,12 @@ vector<T> minMax(vector<T> vect) {
         T rightLow = mid + 1;
 
         vector<T> leftVect;
-        for (T x = 0; x < mid; x++) {
+        for (int x = 0; x < mid; x++) {
             leftVect.push_back(vect[x]);
         }
 
         vector<T> rightVect;
-        for (T x = mid; x < vect.size(); x++) {
+        for (int x = mid; x < vect.size(); x++) {
             rightVect.push_back(vect[x]);
         }
 
@@ -99,6 +100,7 @@ void testFunc(vector<T4> &vect, T4 arr[], int arrSize) {
 
 int main() {
 
+    cout << "Vector 1, size 8" << endl;
     vector<int> vect1;
     int arr1[] = {100, 5, 1, 6, 102, -1, 6, 0};
     int arr1Size = 8;
@@ -108,6 +110,8 @@ int main() {
     printVect(answerVect1);
 
     cout << endl;
+
+    cout << "Vector 2, size 1" << endl;
 
     vector<int> vect2;
     int arr2[] = {100};
@@ -119,6 +123,8 @@ int main() {
 
     cout << endl;
 
+    cout << "Vector 3, size 2" << endl;
+
     vector<int> vect3;
     int arr3[] = {100, 50};
     int arr3Size = 2;
@@ -129,16 +135,19 @@ int main() {
 
     cout << endl;
 
+    cout << "Vector 4, size 7" << endl;
 
     vector<int> vect4;
-    int arr4[] = {100, 504, 241, 54, 2521, 2, 0, 12};
-    int arr4Size = 8;
+    int arr4[] = {100, 504, 241, 54, 2521, 2, 0};
+    int arr4Size = 7;
     fillVect(vect4, arr4, arr4Size);
     printVect(vect4);
     vector<int> answerVect4 = minMax(vect4);
     printVect(answerVect4);
 
     cout << endl;
+
+    cout << "Vector 5, size 8" << endl;
 
     vector<int> vect5;
     int arr5[] = {100, 504, 1000, 200, 12, 2, 0, 11};
@@ -150,15 +159,20 @@ int main() {
 
     cout << endl;
 
+    cout << "Vector 6, size 7" << endl;
+
     vector<int> vect6;
-    int arr6[] = {100, 100, 100, 100, 100, 100, 100, 100};
-    int arr6Size = 8;
+    int arr6[] = {100, 100, 100, 100, 100, 100, 100};
+    int arr6Size = 7;
     fillVect(vect6, arr6, arr6Size);
     printVect(vect6);
     vector<int> answerVect6 = minMax(vect6);
     printVect(answerVect6);
 
     cout << endl;
+
+
+    cout << "Vector 7, size 8" << endl;
 
     vector<int> vect7;
     int arr7[] = {0, 1, 2, 3, 4, 5, 6, 7};
@@ -170,6 +184,9 @@ int main() {
 
     cout << endl;
 
+
+    cout << "Vector 8, size 8" << endl;
+
     vector<int> vect8;
     int arr8[] = {7, 6, 5, 4, 3, 2, 1, 0};
     int arr8Size = 8;
@@ -179,6 +196,9 @@ int main() {
     printVect(answerVect8);
 
     cout << endl;
+
+
+    cout << "Vector 9, size 8" << endl;
 
     vector<int> vect9;
     int arr9[] = {4, 3, 2, 1, 2, 3, 4, 3};
@@ -190,15 +210,21 @@ int main() {
 
     cout << endl;
 
+
+    cout << "Vector 10, size 7" << endl;
+
     vector<double> vect10;
-    double arr10[] = {4.1, 3.9, 2.0, 1.5, 2.1, 3.6, 4.4, 3.1};
-    double arr10Size = 8;
-    fillVect(vect10, arr10, arr9Size);
+    double arr10[] = {4.1, 3.9, 2.0, 1.5, 2.1, 3.6, 4.4};
+    int arr10Size = 7;
+    fillVect(vect10, arr10, arr10Size);
     printVect(vect10);
     vector<double> answerVect10 = minMax(vect10);
     printVect(answerVect10);
 
     cout << endl;
+
+
+    cout << "Vector 11, size 8" << endl;
 
     vector<double> vect11;
     double arr11[] = {4.1, 3.1, 2.1, 1.1, 2.0, 3.1, 4.1, 3.1};
@@ -210,6 +236,8 @@ int main() {
 
     cout << endl;
 
+    cout << "Vector 13, size 8" << endl;
+
     vector<char> vect13;
     char arr13[] = {'y', 't', 'd', 'a', 'e', 'b', 'x', 'p'};
     int arr13Size = 8;
@@ -219,6 +247,8 @@ int main() {
     printVect(answerVect13);
 
     cout << endl;
+
+    cout << "Vector 14, size 8" << endl;
 
     vector<char> vect14;
     char arr14[] = {'z', 'z', 'z', 'z', 'z', 'z', 'z', 'z'};
@@ -230,6 +260,8 @@ int main() {
 
     cout << endl;
 
+    cout << "Vector 15, size 2" << endl;
+
     vector<char> vect15;
     char arr15[] = {'t', 'b'};
     int arr15Size = 2;
@@ -239,6 +271,8 @@ int main() {
     printVect(answerVect15);
 
     cout << endl;
+
+    cout << "Vector 16, size 1" << endl;
 
     vector<char> vect16;
     char arr16[] = {'t'};
@@ -251,6 +285,8 @@ int main() {
 
     cout << endl;
 
+    cout << "Vector 17, size 7" << endl;
+
     vector<int> vect17;
     int arr17[] = {7, 6, 5, 4, 3, 2, 1};
     int arr17Size = 7;
@@ -261,10 +297,12 @@ int main() {
 
     cout << endl;
 
+    cout << "Vector 18, size 7" << endl;
+
     vector<double> vect18;
     double arr18[] = {7.6, 6.1, 5.0, 4.51, 3.623, 1.110, 1.111};
     int arr18Size = 7;
-    testFunc(vect18,arr18,arr18Size);
+    testFunc(vect18, arr18, arr18Size);
 
     cout << endl;
 
