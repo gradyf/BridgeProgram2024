0cc773c1b3b4ce68f595d2ced3b38b972ea0cf68
Gray Forrester <gradyforrester3@gmail.com>
2024-04-25 21:55:52 -0400

    HW13

---
+++ HW15/gnf5628_HW15.cpp
@@ -0,0 +1,19 @@
+//
+// Created by Gray Forrester on 4/25/24.
+//
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Employee {
+    string name;
+    int idNumber;
+    double rate;
+
+};
+
+int main() {
+
+    cout << "TEST";
+}
