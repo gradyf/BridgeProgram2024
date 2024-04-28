bfd0d340390aa3837a6d42215081aa0e2f2efbe3
Gray Forrester <gradyforrester3@gmail.com>
2024-04-28 12:41:30 -0400

    HW15

--- HW15/gnf5628_HW15.cpp
+++ HW15/gnf5628_HW15.cpp
@@ -3,15 +3,17 @@
 //
 #include <iostream>
 #include <string>
+
 using namespace std;
 
 class People {
+public:
     double paid;
     double owed;
     string name;
-public:
+
     People(double amountPaid, string overallName, double amountOwed = 0.00)
-    : paid(amountPaid), name(overallName), owed(amountOwed) {}
+            : paid(amountPaid), name(overallName), owed(amountOwed) {}
 
     People();
 
@@ -23,28 +25,65 @@ public:
 class Node {
 public:
     People person;
-    Node* next;
-    Node(People newPerson = People(), vector<People>* newNext = nullptr) : person(newPerson), next(newNext) {}
+    Node *next;
+
+    Node(People newPerson = People(), Node *newNext = nullptr) : person(newPerson), next(newNext) {}
 
 };
 
 class LList {
-    Node* head;
 public:
-    LList() : head(nullptr) {}
+    Node *head;
+
+    LList(Node *firstHead = nullptr) : head(firstHead) {}
+
     ~LList() {
-        Node* current = head;
+        Node *current = head;
         while (current != nullptr) {
-            Node* nextNode = current->next;
+            Node *nextNode = current->next;
             delete current;
             current = nextNode;
         }
         head = nullptr;
     }
 
+    void printList() {
+        Node *n = head;
+        while (n != nullptr) {
+            cout << n->person.name << " paid " << n->person.paid << endl;
+            n = n->next;
+        }
+    }
+
+    void addNode(Node *n) {
+        Node *current = head;
+        while (n != nullptr) {
+            if (current->next == nullptr) {
+                current->next = n;
+                n->next == nullptr;
+            } else {
+                current = current->next;
+            }
+
+        }
+    }
+
 };
 
 
 int main() {
-    cout << "TEST";
+
+    Node *first = new Node(People(50.12, "John Smith"));
+    Node *second = new Node(People(20.13, "Jane Smith"));
+    Node *third = new Node(People(1.13, "Gray Smith"));
+
+    first->next = second;
+    second->next = third;
+    third->next = nullptr;
+
+    LList *list = new LList(first);
+
+    list->printList();
+    delete list;
+
 }
