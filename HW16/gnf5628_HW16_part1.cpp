843a6e1eb5090f928e074e67508507f145d4ee5f
Gray Forrester <gradyforrester3@gmail.com>
2024-05-07 22:51:35 -0500

    HW16

---
+++ HW16/gnf5628_HW16_part1.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+bool pascalChecker(ifstream &in_file) {
+    char symb;
+    vector<char> tracker;
+
+    while (in_file >> symb) {
+        if (symb == '[') {
+            tracker.push_back(symb);
+        } else if (symb == '{') {
+            tracker.push_back(symb);
+        } else if (symb == '(') {
+            tracker.push_back(symb);
+        } else if (symb == ']') {
+            tracker.pop_back();
+        } else if (symb == '}') {
+            tracker.pop_back();
+        } else if (symb == '(') {
+            tracker.pop_back();
+        }
+
+    }
+
+    if (tracker.empty()) {
+        return true;
+    } else {
+        return false;
+    }
+
+}
+
+void checkerOutput(ifstream &in_file) {
+
+    if (pascalChecker(in_file)) {
+        cout << in_file."Success!";
+    }
+    else {
+
+    }
+
+}
+
+
+int main() {
+    ifstream in_stream;
+
+    in_stream.open("P1_1.txt");
+
+    if(in_stream.fail()) {
+        cout << "The program failed to open the text file.";
+        exit(1);
+
+    }
+    else {
+
+//        cout << "success";
+        cout << pascalChecker(in_stream);
+
+    }
+
+    in_stream.close();
+
+
+
+
+
+}
