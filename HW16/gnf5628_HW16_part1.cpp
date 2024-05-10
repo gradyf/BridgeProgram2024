db8631c47bb1cbdc930814fca063b29746e2ea16
Gray Forrester <gradyforrester3@gmail.com>
2024-05-10 11:10:54 -0500

    HW16

--- HW16/gnf5628_HW16_part1.cpp
+++ HW16/gnf5628_HW16_part1.cpp
@@ -2,31 +2,83 @@
 #include <fstream>
 #include <list>
 
-
 using namespace std;
+
+template<class T>
+class Stack {
+    list<T> data;
+
+public:
+    void push(T newItem) {
+        data.push_back(newItem);
+    }
+
+    void pop() {
+        return data.pop_back();
+    }
+
+    T top() const {
+        return data.back();
+    }
+
+    bool isEmpty() const {
+        return data.size() == 0;
+    }
+
+    int size() const {
+        return data.size();
+    }
+
+    void clear() {
+        data.clear();
+    }
+
+};
 
 const bool pascalChecker(ifstream &in_file) {
     char symb;
-    list<char> tracker;
+    string beg_end;
+    Stack<char> tracker;
 
-    while (in_file >> symb) {
-        if (symb == '[') {
-            tracker.push_back(symb);
-        } else if (symb == '{') {
-            tracker.push_back(symb);
-        } else if (symb == '(') {
-            tracker.push_back(symb);
-        } else if (symb == ']') {
-            tracker.pop_back();
-        } else if (symb == '}') {
-            tracker.pop_back();
-        } else if (symb == '(') {
-            tracker.pop_back();
+    while (in_file >> beg_end) {
+        if (beg_end == "begin") {
+            while (in_file >> symb) {
+                if (symb == 'e') {
+                    in_file >> beg_end;
+                    if (beg_end == "end") {
+                        if (tracker.isEmpty()) {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    } else {
+                        in_file.putback('e');
+                    }
+                } else if (symb == '{' || symb == '(' || symb == '[' || symb == '}' || symb == ')' || symb == ']') {
+                    if (symb == '{' || symb == '(' || symb == '[') {
+                        cout << "Pushing: " << symb << endl;
+                        tracker.push(symb);
+                    } else if (symb == '}' && tracker.top() == '{') {
+                        cout << "Popping: " << symb << endl;
+                        tracker.pop();
+                    } else if (symb == ']' && tracker.top() == '[') {
+                        cout << "Popping: " << symb << endl;
+                        tracker.pop();
+                    } else if (symb == ')' && tracker.top() == '(') {
+                        cout << "Popping: " << symb << endl;
+                        tracker.pop();
+                    } else {
+                        cout << "Pushing: " << symb << endl;
+                        tracker.push(symb);
+                    }
+
+
+                }
+            }
         }
-
     }
 
-    if (tracker.empty()) {
+    if (tracker.isEmpty()) {
         return true;
     } else {
         return false;
@@ -34,13 +86,13 @@ const bool pascalChecker(ifstream &in_file) {
 
 }
 
+
 void checkerOutput(ifstream &in_file) {
 
 
     if (pascalChecker(in_file)) {
         cout << " was a Success!" << endl;
-    }
-    else {
+    } else {
         cout << " was incorrect!" << endl;
     }
 
@@ -54,18 +106,16 @@ int main() {
 
     in_stream.open(test);
 
-    if(in_stream.fail()) {
+    if (in_stream.fail()) {
         cout << "The program failed to open the text file.";
         exit(1);
 
-    }
-    else {
+    } else {
 
-        cout << test;
+        cout << test << endl;
         checkerOutput(in_stream);
 
     }
-
 
 
     in_stream.close();
@@ -76,22 +126,56 @@ int main() {
 
     in_stream.open(test);
 
-    if(in_stream.fail()) {
+    if (in_stream.fail()) {
         cout << "The program failed to open the text file.";
         exit(1);
 
-    }
-    else {
+    } else {
 
-        cout << test;
+        cout << test << endl;
         checkerOutput(in_stream);
 
     }
 
     in_stream.close();
 
+    cout << "----------NEXT TEST----------" << endl;
 
+    test = "P1_3.txt";
 
+    in_stream.open(test);
+
+    if (in_stream.fail()) {
+        cout << "The program failed to open the text file.";
+        exit(1);
+
+    } else {
+
+        cout << test << endl;
+        checkerOutput(in_stream);
+
+    }
+
+    in_stream.close();
+
+    cout << "----------NEXT TEST----------" << endl;
+
+    test = "P1_4.txt";
+
+    in_stream.open(test);
+
+    if (in_stream.fail()) {
+        cout << "The program failed to open the text file.";
+        exit(1);
+
+    } else {
+
+        cout << test << endl;
+        checkerOutput(in_stream);
+
+    }
+
+    in_stream.close();
 
 
 }
